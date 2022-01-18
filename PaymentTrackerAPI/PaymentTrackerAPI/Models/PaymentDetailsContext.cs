6f6685453f5a8e94a9b889a28046cd16c1b9fb00
Jacob Renke <jacob.m.renke@outlook.com>
2022-01-17 19:18:18 -0500

    some initial work

---
+++ PaymentTrackerAPI/PaymentTrackerAPI/Models/PaymentDetailsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentTrackerAPI.Models
+{
+    public class PaymentDetailsContext:DbContext
+    {
+        public PaymentDetailsContext(DbContextOptions<PaymentDetailsContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+    }
+}
